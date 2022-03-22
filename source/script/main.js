a6f14dea7d73280d60823581d0d5ffef1f952e98
Your Name <you@example.com>
2022-03-22 16:33:13 +0200

    add headar shadow on scroll

--- source/script/main.js
+++ source/script/main.js
@@ -2,3 +2,12 @@ const toggleButton = document.querySelector('.dark-light');
 toggleButton.addEventListener("click", ()=>{
     document.body.classList.toggle("dark-mode");
 });
+
+const wrapper = document.querySelector(".wrapper");
+const header = document.querySelector(".header");
+
+wrapper.addEventListener("scroll", (e) => {
+ e.target.scrollTop > 30
+  ? header.classList.add("header-shadow")
+  : header.classList.remove("header-shadow");
+});
